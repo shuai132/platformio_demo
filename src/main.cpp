dea5d1df0ff9c4cff623f95db9f3ffbb345a2cc2
liushuai <770722922@qq.com>
2022-09-06 19:21:30 +0800

    feat: wifi ap test

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,36 @@
 #include <HardwareSerial.h>
+#include <IPv6Address.h>
+#include <WiFiAP.h>
+
+static WiFiAPClass wiFiAPClass;
 
 void setup() {
   Serial.begin(115200);
   pinMode(BLINK_LED, OUTPUT);
+  wiFiAPClass.softAP("ESP", "88888888");
 }
 
 void loop() {
-  Serial.println("loop...");
-  digitalWrite(BLINK_LED, HIGH);
-  delay(100);
-  digitalWrite(BLINK_LED, LOW);
-  delay(100);
+  delay(2000);
+
+  Serial.print("AP IP address: ");
+  Serial.println(wiFiAPClass.softAPIP());
+
+  Serial.print("softAP Broadcast IP: ");
+  Serial.println(wiFiAPClass.softAPBroadcastIP());
+
+  Serial.print("softAP NetworkID: ");
+  Serial.println(wiFiAPClass.softAPNetworkID());
+
+  Serial.print("softAP SubnetCIDR: ");
+  Serial.println(wiFiAPClass.softAPSubnetCIDR());
+
+  Serial.print("softAP Hostname: ");
+  Serial.println(wiFiAPClass.softAPgetHostname());
+
+  Serial.print("softAP macAddress: ");
+  Serial.println(wiFiAPClass.softAPmacAddress());
+
+  Serial.print("softAP StationNum: ");
+  Serial.println(wiFiAPClass.softAPgetStationNum());
 }
