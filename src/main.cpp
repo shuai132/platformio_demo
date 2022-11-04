65d123896d21193763b1d2a6657c4b2c7f4795be
liushuai <770722922@qq.com>
2022-11-04 18:35:52 +0800

    feat: finish rpc test

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,11 @@
 using namespace RpcCore;
 using namespace esp_rpc;
 
+// #define TEST_RPC_CLIENT
+// #define ENABLE_AP_ONLY
+
 static SimpleTimer timer;
+static const short PORT = 8080;
 
 namespace esp_rpc {
 void setTimeout(uint32_t ms, std::function<void()> cb) {
@@ -19,21 +23,63 @@ void setTimeout(uint32_t ms, std::function<void()> cb) {
 }
 }  // namespace esp_rpc
 
-static ESP_WiFiManager wifiManager;
-static std::unique_ptr<rpc_server> server;
-static std::shared_ptr<RpcCore::Rpc> rpc;
-static const short PORT = 8080;
+// 启动函数
+static void test_rpc_client() {
+  static IPAddress StaticIP(192, 168, 178, 3);
+  static IPAddress Gateway(192, 168, 178, 1);
+  static IPAddress SubnetMask(255, 255, 255, 0);
 
-#define ENABLE_AP_ONLY
+  static char ssid[] = "MI9";
+  static char pass[] = "88888888";
 
-static void initWiFi() {
+  Serial.println("Start STA_Mode");
+  WiFi.mode(WIFI_STA);
+  if (WiFi.begin(ssid, pass) == WL_CONNECT_FAILED) {
+    Serial.println("STA_Mode config failed");
+  } else {
+    Serial.println("STA_Mode is config successful");
+  }
+  Serial.println(::String(ssid) + " connecting");
+  WiFi.config(StaticIP, Gateway, SubnetMask);
+  while (WiFi.status() != WL_CONNECTED) {
+    if (WiFi.status() == WL_DISCONNECTED) {
+      Serial.print(".");
+      delay(200);
+    }
+  }
+  Serial.println("WiFi connected");
+  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.macAddress());
+  Serial.println(WiFi.subnetMask());
+  Serial.println(WiFi.gatewayIP());
+  Serial.println(WiFi.SSID());
+  Serial.println(WiFi.psk());
+
+  // rpc client
+  static rpc_client client;
+  client.on_open = [&](const std::shared_ptr<RpcCore::Rpc>& rpc) {
+    LOGE("client: on_open");
+    rpc->cmd("cmd")->msg(RpcCore::String("hello"))->rsp([&](const RpcCore::String& data) {})->call();
+  };
+  client.on_close = [] {
+    LOGE("client: on_close");
+  };
+  client.on_open_failed = [](std::error_code ec) {
+    LOGE("client: on_open_failed: %d", ec.value());
+  };
+  LOGE("client: try open...");
+  client.open("192.168.178.115", PORT);
+}
+
+static void test_rpc_server() {
 #ifdef ENABLE_AP_ONLY
   WiFi.softAP("002", "1029384756");
 #else
   LOGI("start wifi manager...");
+  static ESP_WiFiManager wifiManager;
   wifiManager.setDebugOutput(true);
   wifiManager.setTimeout(10);
-  wifiManager.autoConnect("001");
+  wifiManager.autoConnect("Mi9", "88888888");
 
   LOGI("check connect...");
   if (!WiFi.isConnected()) {
@@ -41,67 +87,56 @@ static void initWiFi() {
     WiFi.softAP("002", "1029384756");
   }
 #endif
-}
 
-static std::string getIp() {
-  if (WiFi.isConnected()) {
-    return WiFi.localIP().toString().c_str();
-  } else {
-    return WiFi.softAPIP().toString().c_str();
-  }
-}
-
-static void dumpWiFiInfo() {
+  static auto getIp = []() -> std::string {
+    if (WiFi.isConnected()) {
+      return WiFi.localIP().toString().c_str();
+    } else {
+      return WiFi.softAPIP().toString().c_str();
+    }
+  };
   LOGI("IP address: %s", getIp().c_str());
   Serial.println();
-}
 
-static void initRpcTask() {
-  rpc->subscribe("cmd", [](const RpcCore::String& data) -> RpcCore::String {
-    LOGI("get cmd: cmd: %s", data.c_str());
-    return "world";
-  });
-}
-
-static void client_test() {
-  // client
-  rpc_client client;
-  client.on_open = [&](const std::shared_ptr<RpcCore::Rpc>& rpc) {
-    rpc->cmd("cmd")->msg(RpcCore::String("hello"))->rsp([&](const RpcCore::String& data) {})->call();
-  };
-  client.on_close = [&] {};
-  client.open("localhost", std::to_string(PORT));
-}
-
-void setup() {
-  Serial.begin(115200);
-
-  // config wifi
-  initWiFi();
-  dumpWiFiInfo();
+  LOGI("start rpc server...");
+  static std::unique_ptr<rpc_server> server;
+  static std::shared_ptr<RpcCore::Rpc> rpc;
 
   // start rpc task
   server = std::make_unique<rpc_server>(PORT);
   server->on_session = [](const std::weak_ptr<rpc_session>& ws) {
-    LOGD("on_session");
+    LOGI("on_session");
 
     if (rpc) {
-      LOGD("rpc exist, will close new session");
+      LOGI("rpc exist, will close new session");
       ws.lock()->close();
       return;
     }
 
     auto session = ws.lock();
     session->on_close = [] {
-      LOGD("session: on_close");
+      LOGI("session: on_close");
       rpc = nullptr;
     };
 
     rpc = session->rpc;
-    initRpcTask();
+    rpc->subscribe("cmd", [](const RpcCore::String& data) -> RpcCore::String {
+      LOGI("get cmd: cmd: %s", data.c_str());
+      return "world";
+    });
   };
-  LOGD("rpc start...");
+  LOGI("rpc start...");
   server->start();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+#ifdef TEST_RPC_CLIENT
+  test_rpc_client();
+  return;
+#endif
+  test_rpc_server();
 }
 
 void loop() {
