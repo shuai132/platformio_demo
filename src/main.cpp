14510bbe4bd59bcba4f15246da8811a15c946baa
liushuai <770722922@qq.com>
2022-11-11 23:06:57 +0800

    feat: midi music player

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,39 @@
+#include <Arduino.h>
 #include <HardwareSerial.h>
 
+#include "fre_table.h"
+#include "midi.h"
+#include "music.h"
+
+#define PLAY_PIN 5
+
+static bool s_enable_note_off;
+static float s_play_speed = 1;
+
+static void notePlayer(const NoteEvent& e) {
+  auto delayMs = (uint16_t)((float)e.tickMs / s_play_speed);
+  if (s_enable_note_off) {
+    if (e.isOn) {
+      tone(PLAY_PIN, (uint16_t)fre_table[e.key - 21], delayMs);
+    } else {
+      tone(PLAY_PIN, 0, delayMs);
+    }
+  } else {
+    if (e.isOn) {
+      tone(PLAY_PIN, (uint16_t)fre_table[e.key - 21]);
+    } else {
+      delay(delayMs);
+    }
+  }
+}
+
 void setup() {
-    Serial.begin(115200);
-    pinMode(BLINK_LED, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
-    Serial.println("loop...");
-    digitalWrite(BLINK_LED, HIGH);
-    delay(100);
-    digitalWrite(BLINK_LED, LOW);
-    delay(100);
+  s_enable_note_off = true;
+  playMusic(SuperMario, sizeof(SuperMario) / sizeof(NoteEvent), notePlayer);
+  s_enable_note_off = false;
+  playMusic(QuanYeCha, sizeof(QuanYeCha) / sizeof(NoteEvent), notePlayer);
 }
---
+++ src/midi.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cstdint>
+#include <functional>
+
+struct NoteEvent {
+  NoteEvent() = default;
+  NoteEvent(uint8_t track, bool isOn, uint8_t key, uint16_t tickMs) : track(track), isOn(isOn), key(key), tickMs(tickMs) {}
+  uint8_t track = 0;
+  bool isOn = false;
+  uint8_t key = 0;
+  uint16_t tickMs = 0;
+};
+
+using NoteHandle = std::function<void(NoteEvent)>;
+
+inline void playMusic(const NoteEvent* music, uint16_t len, const NoteHandle& handle) {
+  for (int i = 0; i < len; ++i) {
+    handle(music[i]);
+  }
+}
