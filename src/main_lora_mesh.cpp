744f969e0ffd4de524ac3a9ee80800a996d81b0c
liushuai <770722922@qq.com>
2025-04-28 22:28:02 +0800

    feat: fit mesh

--- src/main_lora_mesh.cpp
+++ src/main_lora_mesh.cpp
@@ -3,7 +3,11 @@
 #include <Arduino.h>
 #include <RadioLib.h>
 
+#include <string>
+
+#include "SimpleTimer.h"
 #include "log.h"
+#include "mesh_core.hpp"
 
 // NSS pin:   PA4
 // DIO1 pin:  PB4
@@ -11,48 +15,30 @@
 // BUSY pin:  PB14
 static LLCC68 radio = new Module(PA4, PB4, PB15, PB14);
 
-#include "mesh_core.hpp"
+static SimpleTimer timer;
+static std::function<void(std::string)> recv_handle;
+static volatile bool received_flag = false;
+static int send_count = 0;
 
-/**
- * supply your implementation:
- *
- * NOTE:
- * 1. broadcast and recv_handle should ensure packet is complete
- * 2. all methods can be static
- */
 struct Impl {
-  /**
-   * @param data binary data to broadcast
-   */
-  void broadcast(std::string data) {
-    (void)(data);
+  static void broadcast(std::string data) {
+    radio.transmit(data.data(), data.size());
   }
 
-  /**
-   * @param handle store it, call it on receive broadcast.
-   */
-  void set_recv_handle(std::function<void(std::string)> handle) {
-    (void)(handle);
+  static void set_recv_handle(std::function<void(std::string)> handle) {
+    recv_handle = std::move(handle);
   }
 
-  /**
-   * @return millisecond timestamp
-   */
   static mesh_core::timestamp_t get_timestamp_ms() {
-    return 0;
+    return HAL_GetTick();
   }
 
-  /**
-   * @param handle call it after `ms`
-   * @param ms milliseconds
-   */
   static void run_delay(std::function<void()> handle, int ms) {
-    (void)(handle);
-    (void)(ms);
+    timer.setTimeout(ms, std::move(handle));
   }
 };
 
-int mesh_core_test() {
+static int mesh_core_test() {
   Impl impl;
   mesh_core::mesh<Impl> mesh(&impl);
   mesh.set_addr(0x00);
@@ -63,13 +49,64 @@ int mesh_core_test() {
   return 0;
 }
 
-void lora_init() {
-  LOGD("init...");
+static void loop_check_recv() {
+  // check if the flag is set
+  if (received_flag) {
+    // reset flag
+    received_flag = false;
+
+    // you can read received data as an Arduino String
+    String str;
+    int state = radio.readData(str);
+
+    // you can also read received data as byte array
+    /*
+      byte byteArr[8];
+      int numBytes = radio.getPacketLength();
+      int state = radio.readData(byteArr, numBytes);
+    */
+
+    if (state == RADIOLIB_ERR_NONE) {
+      // packet was successfully received
+      LOGD("RECV: %s", str.c_str());
+      LOGD("RSSI: %f dBm", radio.getRSSI());
+      LOGD("SNR: %f dB", radio.getSNR());
+      LOGD("Frequency error: %f Hz", radio.getFrequencyError());
+      if (recv_handle) recv_handle(std::string(str.begin(), str.end()));
+    } else if (state == RADIOLIB_ERR_CRC_MISMATCH) {
+      LOGD("CRC error!");
+    } else {
+      LOGD("recv failed, code %d", state);
+    }
+  }
+}
+
+static void lora_init() {
+  LOGD("lora init...");
   int state = radio.begin();
   if (state == RADIOLIB_ERR_NONE) {
     LOGD("init success");
   } else {
-    LOGD("init failed, code: %d", state);
+    LOGE("init failed, code: %d", state);
+    while (true) {
+      delay(10);
+    }
+  }
+
+  // set the function that will be called when new packet is received
+  radio.setPacketReceivedAction([] {
+    received_flag = true;
+  });
+}
+
+static void lora_start_recv() {
+  // start listening for LoRa packets
+  LOGD("starting to listen...");
+  int state = radio.startReceive();
+  if (state == RADIOLIB_ERR_NONE) {
+    LOGD("start recv: success!");
+  } else {
+    LOGE("start recv: failed, code: %d", state);
     while (true) {
       delay(10);
     }
@@ -77,14 +114,12 @@ void lora_init() {
 }
 
 // counter to keep track of transmitted packets
-int count = 0;
-
-void lora_test() {
+static void lora_send() {
   LOGD("send...");
 
   // you can transmit C-string or Arduino string up to
   // 256 characters long
-  String str = "Hello World! #" + String(count++);
+  String str = "Hello World! #" + String(send_count++);
   int state = radio.transmit(str);
 
   // you can also transmit byte array up to 256 bytes long
@@ -92,7 +127,6 @@ void lora_test() {
     byte byteArr[] = {0x01, 0x23, 0x45, 0x56, 0x78, 0xAB, 0xCD, 0xEF};
     int state = radio.transmit(byteArr, 8);
   */
-
   if (state == RADIOLIB_ERR_NONE) {
     // the packet was successfully transmitted
     LOGD("send: success!");
@@ -101,30 +135,27 @@ void lora_test() {
     // the supplied packet was longer than 256 bytes
     LOGD("send: too long!");
   } else if (state == RADIOLIB_ERR_TX_TIMEOUT) {
-    // timeout occured while transmitting packet
+    // timeout occurred while transmitting packet
     LOGD("send: timeout!");
   } else {
     // some other error occurred
-    LOGD("send: failed, code: %d", state);
+    LOGE("send: failed, code: %d", state);
   }
-
-  // wait for a second before transmitting again
-  delay(1000);
 }
 
 void setup() {
   Serial1.begin(115200);
   pinMode(BLINK_LED, OUTPUT);
   lora_init();
+  lora_start_recv();
+  timer.setInterval(1000, [] {
+    lora_send();
+    lora_start_recv();
+  });
 }
 
 void loop() {
-  LOG("mesh...");
-  digitalWrite(BLINK_LED, HIGH);
-  delay(100);
-  digitalWrite(BLINK_LED, LOW);
-  delay(100);
-  lora_test();
+  timer.run();
 }
 
 #endif
