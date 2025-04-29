93a913017da3d1e41e0ff1f231aac1a79a486f9a
liushuai <770722922@qq.com>
2025-04-29 20:29:40 +0800

    feat: mesh test

--- src/main_lora_mesh.cpp
+++ src/main_lora_mesh.cpp
@@ -10,7 +10,7 @@
 #include "mesh_core.hpp"
 
 #define DEVICE_ID 0x01
-// #define ENABLE_MESH
+#define ENABLE_MESH
 
 // PA config
 #define PA_PIN_VALUE LOW
@@ -60,7 +60,7 @@ static mesh_core::mesh<Impl> mesh(&impl);
 static void mesh_core_init() {
   mesh.set_addr(DEVICE_ID);
   mesh.on_recv([](mesh_core::addr_t addr, const mesh_core::data_t& data) {
-    LOGD("addr: 0x%02X, data: %s", addr, data.c_str());
+    LOGD("mesh: addr: 0x%02X, data: %s", addr, data.c_str());
   });
 }
 
@@ -214,7 +214,7 @@ void setup() {
     uint8_t data[128]{};
     size_t size = snprintf((char*)data, sizeof(data), "Hello from: 0x%02X, %d", DEVICE_ID, send_count++);
 #ifdef ENABLE_MESH
-    mesh.send(0x00, std::string((char*)buffer, size));
+    mesh.send(0x01, std::string((char*)data, size));
 #else
     lora_send(data, size);
 #endif
